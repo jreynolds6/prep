5a9cb82ad2816143a45916d2ed3286989cdb5a77
jreynolds6 <jonathan.reynolds@chk.com>
2012-10-02 12:01:12 -0500

    Pushing new changes

---
+++ source/prep/utility/Range.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prep.utility
+{
+    public interface Range<in Item> : IComparable<>
+    {
+        // how are all the ways you need to look at a range?
+        // >, >=, <, <=, no upper bounds, no lower bounds, 
+        bool lowEnd;
+        bool highEnd;
+
+    }
+}
